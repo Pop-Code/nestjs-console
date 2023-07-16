ebf8a6e8f218acded072afa7efe7b80c41e9a8ee
Rmannn <hermann.alexandre@gmail.com>
2023-07-15 21:40:26 -0400

    remove support of Ora

--- test/helpers.spec.ts
+++
@@ -1,8 +0,0 @@
-import { createSpinner } from '../src/helpers';
-
-describe('Helpers', () => {
-    it('should create an ora spinner', () => {
-        const spinner = createSpinner({ text: 'hello' });
-        expect(spinner).toBeDefined();
-    });
-});
